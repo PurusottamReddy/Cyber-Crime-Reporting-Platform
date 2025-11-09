ff6a5cd851c175cd577b85c38763331b148c428f
Purusottam Reddy <purusottamreddy321@gmail.com>
2025-11-09 13:17:02 +0530

    update vercel analytics import to use react package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import AllReports from './pages/AllReports'
 import LookUp from './pages/LookUp'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
